fffde733299c59b30a6a639538464af59559644b
Afaq Munawar <afaqmunawar12@gmail.com>
2022-08-31 21:27:30 +0100

    updated

--- script.js
+++ script.js
@@ -2,30 +2,39 @@ const question = document.querySelector(".game__question");
 const answers = document.querySelectorAll(".game__answer");
 const submitButton = document.querySelector("#submit");
 const playButton = document.querySelector("#play");
-const questionsContainer = document.querySelector(".game__questions-container")
+const questionsContainer = document.querySelector(".game__questions-container");
+const answerButton1 = document.querySelector("#answer1");
+const answerButton2 = document.querySelector("#answer2");
+const answerButton3 = document.querySelector("#answer3");
+const answerButton4 = document.querySelector("#answer4");
+const scores = document.querySelectorAll(".game__score");
 
-playButton.addEventListener("click", playGame);
-submitButton.addEventListener("click", submitAndNextQuestion);
+const randomiseQuestions
 
-function playGame() {
+const playGame = () => {
     playButton.classList.add('remove');
     questionsContainer.classList.remove('remove');
     submitButton.classList.remove('remove');
+    submitAndNextQuestion();
 }
 
-function submitAndNextQuestion() {
+const submitAndNextQuestion =() => {
 }
 
-function chooseAnswer() {
+const chooseAnswer = () => {
     
 }
 
-questions [{
-    "quizQuestion": "what is 10 + 10?",
-    "A": "20",
-    "B": "15",
-    "C": "25",
-    "D": "100"
+playButton.addEventListener("click", playGame);
+
+const questions = [{
+    quizQuestion: "what is 10 + 10?",
+    answers: [
+        {text: "20", correct: true},
+        {text: "100", correct: false},
+        {text: "10", correct: false},
+        {text: "1", correct: false},
+    ]
 }]
 
  
